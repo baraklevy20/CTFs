cc4643a87884a227ddec557d9d036b930979efd8
Barak <baraklevy20@gmail.com>
2020-03-07 18:21:24 +0200

    Complete 13

---
+++ picoCTF/2019/13/readme.md
@@ -0,0 +1,7 @@
+# Challenge
+Cryptography can be easy, do you know what ROT13 is? cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}
+
+# Solution
+ROT13 is a special case of Caesar cipher with n=13, so we simply add 13 to each char to get the flag.
+
+Flag: `picoCTF{not_too_bad_of_a_problem}`
---
+++ picoCTF/2019/13/sol.py
@@ -0,0 +1,11 @@
+crypt = 'cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}'
+x = []
+y = []
+for i in range(26):
+    x.append(chr(ord('A') + i))
+    x.append(chr(ord('a') + i))
+    y.append(chr(ord('A') + ((i + 13) % 26)))
+    y.append(chr(ord('a') + ((i + 13) % 26)))
+rot13 = str.maketrans(str(x), str(y))
+flag = str.translate(crypt, rot13)
+print(FloatingPointError)
