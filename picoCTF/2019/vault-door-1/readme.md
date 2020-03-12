e33c0d28907d7fec46ffa35813d4895cd15b7168
Barak <baraklevy20@gmail.com>
2020-03-12 21:02:59 +0200

    Finish vault-door-1

---
+++ picoCTF/2019/vault-door-1/readme.md
@@ -0,0 +1,11 @@
+# Challenge
+Can you break into this super secure portal? https://2019shell1.picoctf.com/problem/49886/ (link) or http://2019shell1.picoctf.com:49886
+
+# Solution
+That's a similar challenge to `dont-use-client-side`.
+
+We'll go with the same approach, we'll replace each condition with an assignment. e.g.
+`password.charAt(0)  == 'd' &&` becomes `flag[0] = 'd'`.
+
+
+Flag: `picoCTF{d35cr4mbl3_tH3_cH4r4cT3r5_51e7fd}`
---
+++ picoCTF/2019/vault-door-1/sol.py
@@ -0,0 +1,43 @@
+import re
+
+# password.charAt(0)  == 'd' -> flag[0] = 'd'
+split = 4
+conditions = """password.charAt(0)  == 'd' &&
+               password.charAt(29) == '7' &&
+               password.charAt(4)  == 'r' &&
+               password.charAt(2)  == '5' &&
+               password.charAt(23) == 'r' &&
+               password.charAt(3)  == 'c' &&
+               password.charAt(17) == '4' &&
+               password.charAt(1)  == '3' &&
+               password.charAt(7)  == 'b' &&
+               password.charAt(10) == '_' &&
+               password.charAt(5)  == '4' &&
+               password.charAt(9)  == '3' &&
+               password.charAt(11) == 't' &&
+               password.charAt(15) == 'c' &&
+               password.charAt(8)  == 'l' &&
+               password.charAt(12) == 'H' &&
+               password.charAt(20) == 'c' &&
+               password.charAt(14) == '_' &&
+               password.charAt(6)  == 'm' &&
+               password.charAt(24) == '5' &&
+               password.charAt(18) == 'r' &&
+               password.charAt(13) == '3' &&
+               password.charAt(19) == '4' &&
+               password.charAt(21) == 'T' &&
+               password.charAt(16) == 'H' &&
+               password.charAt(27) == '1' &&
+               password.charAt(30) == 'f' &&
+               password.charAt(25) == '_' &&
+               password.charAt(22) == '3' &&
+               password.charAt(28) == 'e' &&
+               password.charAt(26) == '5' &&
+               password.charAt(31) == 'd';""".splitlines()
+
+
+flag = [None] * 32
+for condition in conditions:
+    m = re.search("password.charAt\((.*)\).*'(.*)'", condition)
+    flag[eval(m.group(1))] = m.group(2)
+print(f'picoCTF{{{"".join(flag)}}}')
